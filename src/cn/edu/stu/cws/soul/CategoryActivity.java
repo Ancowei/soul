d45e7f32e0de614067748c2811ebffe95547eb2a
Ray Fung <posixfung@163.com>
2013-05-22 01:40:44 +0800

    removed useless lines

--- src/cn/edu/stu/cws/soul/CategoryActivity.java
+++ src/cn/edu/stu/cws/soul/CategoryActivity.java
@@ -1,45 +1,34 @@
 package cn.edu.stu.cws.soul;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import android.os.Bundle;
 import android.app.Activity;  
-import android.os.Handler;  
 import android.util.Log;
-import android.view.Gravity;
-import android.view.KeyEvent;  
 import android.view.View;  
-import android.widget.Button;  
 import android.widget.ImageView;
 import android.widget.LinearLayout;  
 import android.widget.ScrollView;  
 import android.widget.TextView;  
-import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View.OnClickListener;
 
 
 public class CategoryActivity extends Activity {
-//	private LinearLayout mLayout;  
-    private ScrollView sView;  
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_category);
-		 sView = (ScrollView) this.findViewById(R.id.scrollViewMain);
-		//ScrollViewAdapter svAdapter = new ScrollViewAdapter(this, this);
 		EssayType type = new EssayType();
 		Intent intent = getIntent();
 		type.ID = intent.getIntExtra("catID", 1);
-		
+
 		LinearLayout layout = (LinearLayout)this.findViewById(R.id.svLinearLayout);
 		DBHelper helper = new DBHelper(this, "essay.db");
 		List<Essay> essays;
 		if(type.ID == -1) essays = helper.getEssayFromCollection();
 		else essays = helper.getEssay(type);
-//		essays.add(et); //essay collection
 		Log.d("Cat", String.valueOf(essays.size()));
 		for(int i = 0; i < essays.size(); ++i) {
 			final Essay e = essays.get(i);
@@ -57,7 +46,6 @@ public class CategoryActivity extends Activity {
 			);
 			TextView textView = new TextView(this);
 			textView.setText(e.Content);
-			
 			layout.addView(imgView);
 			layout.addView(textView);
 		}
