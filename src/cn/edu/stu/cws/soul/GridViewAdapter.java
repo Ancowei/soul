8463281e403bb95e66a73e09c782f4727a1125d7
Ray Fung <posixfung@163.com>
2013-05-21 23:01:31 +0800

    fix bug: null pointer exception.

--- src/cn/edu/stu/cws/soul/GridViewAdapter.java
+++ src/cn/edu/stu/cws/soul/GridViewAdapter.java
@@ -1,10 +1,10 @@
 package cn.edu.stu.cws.soul;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
@@ -19,13 +19,17 @@ public class GridViewAdapter extends BaseAdapter {
 	GridViewAdapter(Context c) {
 		this.context = c;
 		DBHelper helper = new DBHelper(this.context, "essay.db");
+		buttons = new ArrayList<ImageButton>();
 		types = helper.getEssayTypes();
-		for(int i = 0; i <= types.size(); ++i) {
+		for(int i = 0; i < types.size(); ++i) {
+			final EssayType t = types.get(i);
 			ImageButton imgButton = new ImageButton(this.context);
 			imgButton.setImageResource(R.drawable.ic_launcher);
 			imgButton.setOnClickListener(new OnClickListener(){
 				public void onClick(View view) {
 					Intent intent = new Intent(view.getContext(), CategoryActivity.class);
+					intent.putExtra("catID", t.ID);
+					view.getContext().startActivity(intent);
 				}
 			}
 			);
